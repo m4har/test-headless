d0124722a04e2857d8476dfde1c45b1730848864
mahardika <mahardicka404@gmail.com>
2019-08-17 10:17:42 +0700

    bisaa

--- BgMessage.js
+++ BgMessage.js
@@ -6,6 +6,5 @@ import type { RemoteMessage } from 'react-native-firebase';
 export default async (message: RemoteMessage) => {
   // handle your message
     console.log('headless', {message})
-
-    return Promise.resolve();
+    return Promise.resolve(message);
 }
